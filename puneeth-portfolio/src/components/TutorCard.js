0c017601b5fc8396ebaf11505f6dd0ae05d2ac09
Puneeth P <puneeth1996p@gmail.com>
2021-03-27 22:56:19 +0530

    time to test the waters

--- puneeth-portfolio/src/components/TutorCard.js
+++ puneeth-portfolio/src/components/TutorCard.js
@@ -30,6 +30,34 @@ export default class TutorCard extends Component {
                         <a href="mailto: puneeth1996trainer@gmail.com?cc=puneeth1996p@gmail.com" target="_blank"  rel="noopener noreferrer"  style={{color: "black", "textDecoration": "none"}}  > <RiMailSendLine    style = {{ width : 25, height : 25 }}  /> Inquire </a>   
                     </Button>
                 </div>
+                <div style={divStyle}>
+                    <h3  style={{fontSize: "2.5vw", marginTop: '0.5vh',}}  >Complete Python Training</h3>
+                    <h3  style={{fontSize: "2.5vw", marginTop: '3vh',}}  >$100(2months duration, 30Hrs). In-person Online class</h3>
+                    <div style={{fontSize: "1.75vw", marginTop: '5vh',}} >
+                        Learn all the major topics in python starting from basic and all the way up to object oriented technique
+                        <br />
+                        Learn by building projects using Database like Sql, Networking Application with Sockets, Visualization Libraries Like Matplotlib, Web Scrapping and many more 
+                        <br />
+                        Hope to see you in this amazing course, its a data driven world and python is the exact vehicle that you should choose to go about solving large scale data driven problems.
+                    </div>
+                    <Button variant="info"     style = {{ margin:"3vh 0 0 0.5vw" }}  >
+                        <a href="mailto: puneeth1996trainer@gmail.com?cc=puneeth1996p@gmail.com" target="_blank"  rel="noopener noreferrer"  style={{color: "black", "textDecoration": "none"}}  > <RiMailSendLine    style = {{ width : 25, height : 25 }}  /> Inquire </a>   
+                    </Button>
+                </div>
+                <div style={divStyle}>
+                    <h3  style={{fontSize: "2.5vw", marginTop: '0.5vh',}}  >Complete Python Training</h3>
+                    <h3  style={{fontSize: "2.5vw", marginTop: '3vh',}}  >$100(2months duration, 30Hrs). In-person Online class</h3>
+                    <div style={{fontSize: "1.75vw", marginTop: '5vh',}} >
+                        Learn all the major topics in python starting from basic and all the way up to object oriented technique
+                        <br />
+                        Learn by building projects using Database like Sql, Networking Application with Sockets, Visualization Libraries Like Matplotlib, Web Scrapping and many more 
+                        <br />
+                        Hope to see you in this amazing course, its a data driven world and python is the exact vehicle that you should choose to go about solving large scale data driven problems.
+                    </div>
+                    <Button variant="info"     style = {{ margin:"3vh 0 0 0.5vw" }}  >
+                        <a href="mailto: puneeth1996trainer@gmail.com?cc=puneeth1996p@gmail.com" target="_blank"  rel="noopener noreferrer"  style={{color: "black", "textDecoration": "none"}}  > <RiMailSendLine    style = {{ width : 25, height : 25 }}  /> Inquire </a>   
+                    </Button>
+                </div>
             </>
         )
     }
--- puneeth-portfolio/src/pages/AboutPage.js
+++ puneeth-portfolio/src/pages/AboutPage.js
@@ -11,17 +11,17 @@ function AboutPage(props) {
             <Hero title={props.title} />
 
             <Content>
-            <p>Hello, my name is <Link to="/">Puneeth</Link>.</p>
-            
-            <p>I'm a full stack engineer with experience in WordPress, PHP, ReactJS, Express JS, Node JS, Oracle SQL, MongoDB, and MySQL.</p>
+                <p>Hello, my name is <Link to="/">Puneeth</Link>.</p>
+                
+                <p>I'm a full stack engineer with experience in WordPress, PHP, ReactJS, Express JS, Node JS, Oracle SQL, MongoDB, and MySQL.</p>
 
-            <p>My dream is to one day start my own business and become an entrepreneur (currently working on some ideas).</p>
+                <p>My dream is to one day start my own business and become an entrepreneur (currently working on some ideas).</p>
 
-            <p>I'm constantly learning new things. currently those things include gaining more experience with MongoDB, React, Express JS, and Node JS</p>
+                <p>I'm constantly learning new things. currently those things include gaining more experience with MongoDB, React, Express JS, and Node JS</p>
 
-            <p>You can check out, all my latest <Link to="/projects">Projects</Link>. Or some interesting <Link to="/projects">Blogs.</Link></p>
+                <p>You can check out, all my latest <Link to="/projects">Projects</Link>. Or some interesting <Link to="/projects">Blogs.</Link></p>
 
-            <p>When I'm not learning something new, I play basketball <span role="img" aria-labelledby="smiley">😁</span>.</p>
+                <p>When I'm not learning something new, I play basketball <span role="img" aria-labelledby="smiley">😁</span>.</p>
             </Content>
         </div>
     );
